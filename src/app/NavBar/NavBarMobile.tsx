84ecb8f93772487fe0642cabaf243435f02bf89e
Lukáš Peťko <luke.petko@gmail.com>
2023-05-13 13:07:59 +0200

    making mobilenav fullscreen WIP

--- src/app/NavBar/NavBarMobile.tsx
+++ src/app/NavBar/NavBarMobile.tsx
@@ -9,10 +9,10 @@ export default function NavBarMobile() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="md:hidden fixed bottom-0 flex flex-col w-screen justify-center items-center p-2 gap-2">
+    <div className="md:hidden fixed bottom-0 flex flex-col w-screen justify-center items-center">
       <div
-        className={`w-96 bg-neutral-950 rounded-lg p-2 mt-2 transition-all duration-700 text-white ${
-          isOpen ? "h-auto opacity-100" : "opacity-0 overflow-hidden"
+        className={`w-screen h-screen bg-neutral-950 p-2 transition-all duration-700 text-white ${
+          isOpen ? "opacity-100" : "opacity-0 overflow-hidden"
         }`}
       >
         <h1 className="text-md font-black uppercase pt-2 pl-3 pb-8 text-neutral-200">
@@ -22,21 +22,17 @@ export default function NavBarMobile() {
         <div className="pt-8 pb-2 px-3">
           <h2 className="text-lg">Lukas</h2>
         </div>
+        <button
+          className="w-max bg-neutral-950 rounded-full p-3"
+          onClick={() => setIsOpen(!isOpen)}
+        ></button>
       </div>
       <button
-        className="w-max bg-neutral-950 rounded-full p-3"
+        className="fixed bottom-2 w-max bg-neutral-950 rounded-full p-3 mb-2"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <X
-          className={`text-4xl text-neutral-400 bg-neutral-950 transition duration-700 ${
-            !isOpen ? "h-0 opacity-0 -rotate-90" : "opacity-100"
-          }`}
-        />
-        <Menu
-          className={`text-4xl text-neutral-400 bg-neutral-950 transition duration-700 ${
-            isOpen ? "h-0 opacity-0 -rotate-90" : "opacity-100"
-          }`}
-        />
+        <X className="text-4xl text-neutral-400 bg-neutral-950 transition duration-700" />
+        <Menu className="text-4xl text-neutral-400 bg-neutral-950 transition duration-700" />
       </button>
     </div>
   );
